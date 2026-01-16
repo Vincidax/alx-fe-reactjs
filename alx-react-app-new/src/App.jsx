14dc3f258d67bf7e3c74ccb25a4177b0253b64f4
vincidax <vincentdx18@gmail.com>
2026-01-16 11:36:45 +0200

    Adding Counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,27 +1,24 @@
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <>
       <div>
         <Header />
-        <MainContent />
         <WelcomeMessage />
-        <UserProfile 
-        name="Alice"
-        age="25"
-        bio="Loves hiking and photography"
-        />
+        <MainContent />
+        <Counter />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <Footer />
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
