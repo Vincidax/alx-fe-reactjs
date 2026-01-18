d4e3f3749da006e4cb0af581fbb6607ed4c6198d
vincidax <vincentdx18@gmail.com>
2026-01-18 16:29:06 +0200

    my-company

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: "1rem", borderBottom: "1px solid #ccc" }}>
+      <Link to="/" style={{ marginRight: "1rem" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ marginRight: "1rem" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ marginRight: "1rem" }}>
+        Services
+      </Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
