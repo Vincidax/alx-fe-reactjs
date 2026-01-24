c52c960a2e9e891afb9491889896037cb64d8af6
vincidax <vincentdx18@gmail.com>
2026-01-24 16:58:54 +0200

    creating recipe-sharing-app routes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div>
+    <Router>
       <h1>Recipe Sharing App</h1>
 
       <Routes>
@@ -20,7 +20,7 @@ function App() {
         />
         <Route path="/recipes/:id" element={<RecipeDetails />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
 
