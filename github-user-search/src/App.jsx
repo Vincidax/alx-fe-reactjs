3a7127edb507aea334b71a102c723058e17fd30a
vincidax <vincentdx18@gmail.com>
2026-01-27 20:06:08 +0200

    github api

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>GitHub User Search</h1>
+      <p>Project setup completed 🚀</p>
+    </div>
+  );
+}
+
+export default App;
