13b51d69b5cb1068e87ac8093e4c2065df1b4d76
vincidax <vincentdx18@gmail.com>
2026-01-27 20:20:49 +0200

    adding axios

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,11 @@
-// import { useState } from "react";
+import React from "react";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>GitHub User Search</h1>
-      <p>Project setup completed 🚀</p>
+      <Search />
     </div>
   );
 }
--- github-user-search/src/services/githubApi.js
+++ github-user-search/src/services/githubApi.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
-const githubApi = axios.create({
+const api = axios.create({
   baseURL: "https://api.github.com",
   headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
+    Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
   },
 });
 
 export const searchUsers = async (query) => {
-  const response = await githubApi.get(`/search/users?q=${query}`);
+  const response = await api.get(`/search/users?q=${query}`);
   return response.data.items;
+};
+
+export const getUserDetails = async (username) => {
+  const response = await api.get(`/users/${username}`);
+  return response.data;
 };
